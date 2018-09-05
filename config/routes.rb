691f7d373ea7040091ec49d8a2e3829c7ddc1db2
Grzegorz Gałęzowski <gsgalezowski@gmail.com>
2018-09-05 12:49:13 +0200

    Add About and Terms pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
+  get 'fajna_strona', to: 'home#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'home#about'
+  get 'terms', to: 'home#terms'
+
 end
